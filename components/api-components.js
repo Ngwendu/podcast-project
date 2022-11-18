bfb0e530e2c5b68d012493ade9a30a03e2065cd4
Ngwendu <gambungwendu@gmail.com>
2022-11-18 19:34:13 +0200

    Updated Podcast

--- components/api-components.js
+++ components/api-components.js
@@ -1,84 +1,84 @@
-const listHtml = document.querySelector("#list");
+// const listHtml = document.querySelector("#list");
 
-const getAllPodcasts = async () => {
-  listHtml.innerHtml = `Loading...`;
-  console.log(5);
+// const getAllPodcasts = async () => {
+//   listHtml.innerHtml = `Loading...`;
+//   console.log(5);
 
-  const response = await fetch("https://podcast-api.netlify.app/shows");
+//   const response = await fetch("https://podcast-api.netlify.app/shows");
 
-  if (!response.ok) {
-    listHtml.innerText = "Something went wrong!";
-    return;
-  }
+//   if (!response.ok) {
+//     listHtml.innerText = "Something went wrong!";
+//     return;
+//   }
 
-  /**
-   * @type {preview[]}
-   */
-  const data = await response.json();
+//   /**
+//    * @type {preview[]}
+//    */
+//   const data = await response.json();
 
-  let newHtml = "";
+//   let newHtml = "";
 
-  for (const { id, title, seasons } of data) {
-    newHtml = `
-            ${newHtml}
-                    <li>
-                        <button data-preview-button="${id}">${title}</button>
-                        <span>(${seasons})</span>
-                    </li>
-        `;
-  }
+//   for (const { id, title, seasons } of data) {
+//     newHtml = `
+//             ${newHtml}
+//                     <li>
+//                         <button data-preview-button="${id}">${title}</button>
+//                         <span>(${seasons})</span>
+//                     </li>
+//         `;
+//   }
 
-  listHtml.innerHTML = newHtml;
-};
+//   listHtml.innerHTML = newHtml;
+// };
 
-/**
- * @param {string} id
- */
-const getSinglePodcast = async (id) => {
-  listHtml, (innerHtml = `Loading...`);
+// /**
+//  * @param {string} id
+//  */
+// const getSinglePodcast = async (id) => {
+//   listHtml, (innerHtml = `Loading...`);
 
-  const response = await fetch(`https://podcast-api.netlify.app/id/${id}`);
+//   const response = await fetch(`https://podcast-api.netlify.app/id/${id}`);
 
-  if (!response.ok) {
-    listHtml.innerHtml = "Something wrong!";
-    return;
-  }
+//   if (!response.ok) {
+//     listHtml.innerHtml = "Something wrong!";
+//     return;
+//   }
 
-  /**
-   * @type {show}
-   */
-  const data = await response.json();
+//   /**
+//    * @type {show}
+//    */
+//   const data = await response.json();
 
-  let seasonsList = "";
+//   let seasonsList = "";
 
-  for (const { image, title } of data.seasons) {
-    seasonsList = `
-            ${seasonsList}
-            
-            <li>
-                <img src="${image}" width="100" height="100">
-                ${title}
-            </li>
-        `;
-  }
+//   for (const { image, title } of data.seasons) {
+//     seasonsList = `
+//             ${seasonsList}
 
-  listHtml.innerHTML = `
-        <button data-action="back"> Back</button>
-        <h2>${data.title}</h2>
-        <ul>${seasonsList}</ul>
-    `;
-};
+//             <li>
+//                 <img src="${image}" width="100" height="100">
+//                 ${title}
+//             </li>
+//         `;
+//   }
 
-document.body.addEventListener("click", (event) => {
-  const { previewButton, action } = event.target.dataset;
+//   listHtml.innerHTML = `
+//         <button data-action="back"> Back</button>
+//         <h2>${data.title}</h2>
+//         <ul>${seasonsList}</ul>
+//     `;
+// };
 
-  if (action && action === "back") {
-    getAllPodcasts();
-    return;
-  }
+// document.body.addEventListener("click", (event) => {
+//   const { previewButton, action } = event.target.dataset;
 
-  if (!previewButton) return;
-  getSinglePodcast(previewButton);
-});
+//   if (action && action === "back") {
+//     getAllPodcasts();
+//     return;
+//   }
 
-getAllPodcasts();
+//   if (!previewButton) return;
+//   getSinglePodcast(previewButton);
+// });
+
+// getAllPodcasts();
--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-import "./components/api-components.js";
+// import "./components/api-components.js";
+
+import "./components/podcast-app.js";
+import "./components/podcast-view-list.js";
+import "./components/podcast-view-single.js";
+import "./components/podcast-controls.js";
